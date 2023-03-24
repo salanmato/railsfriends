ebaf67acc1e8d31fa371e8665c52fd4aed6b74a9
Alan Matos <salanmato@gmail.com>
2023-03-24 16:23:07 -0300

    initial commit

---
+++ config/routes.rb
@@ -0,0 +1,11 @@
+Rails.application.routes.draw do
+  resources :friends
+  #root 'home#index'
+  root 'friends#index'
+  get 'home/about'
+  devise_for :users, path: 'auth', path_names: { sign_in: 'login', sign_out: 'logout', password: 'secret', confirmation: 'verification', unlock: 'unblock', registration: 'register', sign_up: 'cmon_let_me_in' }
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+end
